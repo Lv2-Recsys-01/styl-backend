bf38052843873527966905ac3638283713497830
DarrenKwonDev <darrenkwondev46@gmail.com>
2023-07-03 13:01:00 +0900

    feat: cors middleware

--- src/main.py
+++ src/main.py
@@ -1,8 +1,24 @@
 from typing import Union
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://localhost:8000",
+]
+
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
