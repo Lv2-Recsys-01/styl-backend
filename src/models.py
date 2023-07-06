82c968629217f7d7dca85c5e7b3003ebbd27739f
leehanjeong <rosy0111@naver.com>
2023-07-06 15:39:35 +0900

    chore: add relationships, columns

--- src/models.py
+++ src/models.py
@@ -15,6 +15,12 @@ class User(Base):
     login_id = Column(String, unique=True, index=True)
     login_pwd = Column(String)
 
+    likes = relationship("Like", back_populates="user")
+    clicks = relationship("Click", back_populates="user")
+    staytime = relationship("Staytime", back_populates="user")
+
+
+
     def verify_password(self, plain_password):
         return pwd_context.verify(plain_password, self.login_pwd)
 
@@ -22,60 +28,68 @@ class User(Base):
         self.login_pwd = pwd_context.hash(plain_password)
 
 
-# class User(Base):
-#     __tablename__ = "user"
-
-#     user_id = Column(Integer, primary_key=True, index=True)
-#     username = Column(String, unique=True, index=True)
-#     password = Column(String)
-
-
 class Outfit(Base):
-    __tablename__ = "outfit"
+    __tablename__ = "outfits"
 
     outfit_id = Column(Integer, primary_key=True, index=True)
     gender = Column(String)
+    age = Column(Integer)
     img_url = Column(String)
-    date = Column(DateTime, nullable=False)
+    date = Column(DateTime, nullable=False) # 촬영일
     reporter = Column(String)
     style = Column(String)
     origin_url = Column(String)
+    tags = Column(ARRAY(String))
+    brands = Column(ARRAY(String))
+    region = Column(String)
+    occupation = Column(String)
+    style = Column(String)
+
+    likes = relationship("Like", back_populates="outfit")
+    clicks = relationship("Click", back_populates="outfit")
+    staytimes = relationship("Staytime", back_populates="outfit")
+    similars = relationship("Similar", back_populates="outfit")
+
 
 
 class Like(Base):
     __tablename__ = "like"
 
     user_id = Column(Integer, ForeignKey("user.user_id"), primary_key=True)
-    user = relationship("User")
     outfit_id = Column(Integer, ForeignKey("outfit.outfit_id"), primary_key=True)
-    outfit = relationship("Outfit")
     timestamp = Column(DateTime, nullable=False)
+
+    user = relationship("User", back_populates="likes")
+    outfit = relationship("Outfit", back_populates="likes")
 
 
 class Click(Base):
     __tablename__ = "click"
 
     user_id = Column(Integer, ForeignKey("user.user_id"), primary_key=True)
-    user = relationship("User")
     outfit_id = Column(Integer, ForeignKey("outfit.outfit_id"), primary_key=True)
-    outfit = relationship("Outfit")
     cnt = Column(Integer)
     timestamp = Column(DateTime, nullable=False)
+
+    user = relationship("User", back_populates="clicks")
+    outfit = relationship("Outfit", back_populates="clicks")
 
 
 class Staytime(Base):
     __tablename__ = "staytime"
 
     user_id = Column(Integer, ForeignKey("user.user_id"), primary_key=True)
-    user = relationship("User")
     outfit_id = Column(Integer, ForeignKey("outfit.outfit_id"), primary_key=True)
-    outfit = relationship("Outfit")
     staytime = Column(Float)
+
+    user = relationship("User", back_populates="staytimes")
+    outfit = relationship("Outfit", back_populates="staytimes")
 
 
 class Similar(Base):
     __tablename__ = "similar"
 
     outfit_id = Column(Integer, ForeignKey("outfit.outfit_id"), primary_key=True)
-    outfit = relationship("Outfit")
     similar_outfits = Column(ARRAY(Integer))
+
+    outfit = relationship("Outfit", back_populates="similars")
