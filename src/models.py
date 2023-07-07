ef6779999d9e44ec8a4f0630fc52f273d9a3f7af
leehanjeong <rosy0111@naver.com>
2023-07-07 10:36:03 +0900

    chore: add session_id column, rename tablename

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 from passlib.context import CryptContext
 from sqlalchemy import (ARRAY, Column, DateTime, Float, ForeignKey, Integer,
-                        String, Char)
+                        String, CHAR)
 from sqlalchemy.orm import relationship
 
 from .database import Base
@@ -9,7 +9,7 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "user"
 
     user_id = Column(Integer, primary_key=True, index=True)
     login_id = Column(String, unique=True, index=True) # -> index 필요??
@@ -17,7 +17,7 @@ class User(Base):
 
     likes = relationship("Like", back_populates="user")
     clicks = relationship("Click", back_populates="user")
-    staytime = relationship("Staytime", back_populates="user")
+    staytimes = relationship("Staytime", back_populates="user")
 
     def verify_password(self, plain_password):
         return pwd_context.verify(plain_password, self.login_pwd)
@@ -27,21 +27,20 @@ class User(Base):
 
 
 class Outfit(Base):
-    __tablename__ = "outfits"
+    __tablename__ = "outfit"
 
     outfit_id = Column(Integer, primary_key=True, index=True)
-    gender = Column(Char(2))
-    age = Column(Integer)
+    gender = Column(CHAR(2))
+    age = Column(Integer(4))
     img_url = Column(String)
-    date = Column(DateTime, nullable=False)
-    reporter = Column(String)
-    style = Column(String)
     origin_url = Column(String)
+    reporter = Column(String)
     tags = Column(ARRAY(String))
     brands = Column(ARRAY(String))
     region = Column(String)
     occupation = Column(String)
     style = Column(String)
+    date = Column(DateTime, nullable=False)
 
     likes = relationship("Like", back_populates="outfit")
     clicks = relationship("Click", back_populates="outfit")
@@ -52,6 +51,7 @@ class Outfit(Base):
 class Like(Base):
     __tablename__ = "like"
 
+    session_id = Column(String, primary_key=True)
     user_id = Column(Integer, ForeignKey("user.user_id"), primary_key=True)
     outfit_id = Column(Integer, ForeignKey("outfit.outfit_id"), primary_key=True)
     timestamp = Column(DateTime, nullable=False)
