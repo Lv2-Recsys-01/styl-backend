77a2fc81b428c594f3d9a0f1c47f2ebb13d80eb7
DarrenKwonDev <darrenkwondev46@gmail.com>
2023-07-09 17:24:50 +0900

    feat: connect front & backend in makeshift

--- client/src/hoc/withRouter.jsx
+++ client/src/hoc/withRouter.jsx
@@ -1,10 +1,26 @@
+import { useEffect } from "react";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
 
 function withRouter(Component) {
     function ComponentWithRouterProp(props) {
         let location = useLocation();
         let navigate = useNavigate();
         let params = useParams();
+
+        useEffect(() => {
+            console.log("changed!");
+
+            const checkAuth = async () => {
+                const res = await axios.get("http://localhost:8000/");
+
+                console.log(res);
+            };
+
+            checkAuth();
+            // user info should save in context api
+        }, [location.pathname]);
+
         return <Component {...props} location={location} params={params} navigate={navigate} />;
     }
 
