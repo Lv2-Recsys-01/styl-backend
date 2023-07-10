f6d1eb47b4e74137a1fff37ac5e38f0d3d100775
leehanjeong <rosy0111@naver.com>
2023-07-10 10:52:32 +0900

    chore: add initializing sql file when docker container starts

--- docker-entrypoint-initdb.d/init.sql
+++ docker-entrypoint-initdb.d/init.sql
@@ -1,1 +1,70 @@
--- CREATE DATABASE testdb;
+-- 디비버같은 툴로 확인하기
+
+DROP TABLE IF EXISTS "user";
+CREATE TABLE "user" (
+    user_id SERIAL NOT NULL,
+    user_name VARCHAR UNIQUE,
+    user_pwd VARCHAR,
+    PRIMARY KEY (user_id)
+);
+
+
+DROP TABLE IF EXISTS outfit;
+CREATE TABLE outfit (
+        outfit_id SERIAL NOT NULL,
+        img_url VARCHAR,
+        gender CHAR(1),
+        age INTEGER,
+        origin_url VARCHAR,
+        reporter VARCHAR,
+        tags VARCHAR[],
+        brands VARCHAR[],
+        region VARCHAR,
+        occupation VARCHAR,
+        style VARCHAR,
+        "date" TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+        PRIMARY KEY (outfit_id)
+);
+
+DROP TABLE IF EXISTS "like";
+CREATE TABLE "like" (
+        like_id SERIAL NOT NULL,
+        session_id VARCHAR REFERENCES "session" (session_id),
+        user_id INTEGER REFERENCES "user" (user_id),
+        outfit_id INTEGER REFERENCES outfit (outfit_id),
+        timestamp TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+        is_deleted BOOLEAN,
+        PRIMARY KEY (like_id)
+);
+
+DROP TABLE IF EXISTS click;
+CREATE TABLE click (
+        click_id SERIAL NOT NULL,
+        session_id VARCHAR REFERENCES "session" (session_id),
+        user_id INTEGER REFERENCES "user" (user_id),
+        outfit_id INTEGER REFERENCES outfit (outfit_id),
+        timestamp TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+        duration_seconds INTEGER,
+        PRIMARY KEY (click_id)
+);
+
+DROP TABLE IF EXISTS similar;
+CREATE TABLE similar (
+        outfit_id INTEGER REFERENCES outfit (outfit_id),
+        similar_outfits INTEGER[],
+        PRIMARY KEY (outfit_id)
+);
+
+DROP TABLE IF EXISTS "session";
+CREATE TABLE "session" (
+        session_id VARCHAR PRIMARY KEY,
+        user_id INTEGER REFERENCES "user" (user_id),
+        created_at TIMESTAMP WITHOUT TIME ZONE,
+        expired_at TIMESTAMP WITHOUT TIME ZONE
+);
+
+INSERT INTO "user" (user_name, user_pwd)
+VALUES ('guest', 'guest_pwd');
+
+COPY outfit (gender, age, img_url, origin_url, reporter, tags, brands, region, occupation, style, "date")
+FROM '../meta_22-23.csv' DELIMITER ',' CSV HEADER;
