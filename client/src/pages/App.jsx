5ba3c41771a699080929e2940bf051a8744eb4cc
junwon <jjuny9798@gmail.com>
2023-07-13 03:38:51 +0900

    feat: handle router

--- client/src/pages/App.jsx
+++ client/src/pages/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { useNavigate, BrowserRouter, Outlet, Route, Routes, useParams } from "react-router-dom";
+import { BrowserRouter, Outlet, Route, Routes, useParams } from "react-router-dom";
 import Journey from "./Journey";
 import EntryPage from "./EntryPage";
 import MyPage from "./MyPage";
 import DetailPage from "./DetailPage";
 import Layout from "../components/Layout";
+import NotFoundPage from "./NotFoundPage";
 
 function LayoutOutlet() {
     return (
@@ -23,16 +24,24 @@ function App() {
                     <Route path="/journey" element={<Journey />} />
                     <Route path="/collections" element={<MyPage />} />
                     <Route path="/detail/:outfit_id" element={<DetailPageWrapper />} />
+                    <Route path="*" element={<NotFoundPage />} />
                 </Route>
             </Routes>
         </BrowserRouter>
     );
 }
-//TODO: detail/ede같이 숫자가 아니거나 범위를 넘어갔을 때 처리
+
 function DetailPageWrapper() {
     const { outfit_id } = useParams();
 
-    return <DetailPage outfitId={outfit_id} />;
+    const isNumeric = /^\d+$/.test(outfit_id);
+
+    if (!isNumeric) {
+
+        return <NotFoundPage />;
+    }
+
+    return <DetailPage outfitId={parseInt(outfit_id)} />;
 }
 
 export default App;
