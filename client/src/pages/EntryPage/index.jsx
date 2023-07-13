a69cb7ea843ef209a870d340cf38c6b9e1f0156f
junwon <jjuny9798@gmail.com>
2023-07-13 13:54:17 +0900

    chore: change entry image

--- client/src/pages/EntryPage/index.jsx
+++ client/src/pages/EntryPage/index.jsx
@@ -10,56 +10,55 @@ function EntryBackground() {
                 <Col span={12}>
                     <img
                         className="up"
-                        src="https://image.msscdn.net/mfile_s01/_street_images/21754/list.street_list_56399249891e5.jpg.250?20200818233041"
-                        alt="NoImg"
+                        src="https://codidatabucket.s3.ap-northeast-2.amazonaws.com/img/2023female/91725.jpg"
                     />
                 </Col>
                 <Col span={12}>
                     <img
                         className="down"
-                        src="https://image.msscdn.net/mfile_s01/_street_images/62145/list.street_list_5efaaa72a09ed.jpg.250?20200819103705"
+                        src="https://codidatabucket.s3.ap-northeast-2.amazonaws.com/img/2022male/85039.jpg"
                         alt="NoImg"
                     />
                 </Col>
                 <Col span={12}>
                     <img
                         className="up"
-                        src="https://image.msscdn.net/mfile_s01/_street_images/53328/list.street_list_5d196dd580985.jpg.250?20200819075906"
+                        src="https://codidatabucket.s3.ap-northeast-2.amazonaws.com/img/2022male/82356.jpg"
                         alt="NoImg"
                     />
                 </Col>
                 <Col span={12}>
                     <img
                         className="down"
-                        src="https://image.msscdn.net/mfile_s01/_street_images/53082/list.street_list_5d105346caee9.jpg.250?20200819075435"
+                        src="https://codidatabucket.s3.ap-northeast-2.amazonaws.com/img/2023female/92055.jpg"
                         alt="NoImg"
                     />
                 </Col>
                 <Col span={12}>
                     <img
                         className="up"
-                        src="https://image.msscdn.net/mfile_s01/_street_images/45508/list.street_list_5b48490710aaa.jpg.250?20200819054633"
+                        src="https://codidatabucket.s3.ap-northeast-2.amazonaws.com/img/2023female/92003.jpg"
                         alt="NoImg"
                     />
                 </Col>
                 <Col span={12}>
                     <img
                         className="down"
-                        src="https://image.msscdn.net/mfile_s01/_street_images/80432/list.street_list_624baf2c355f6.jpg.250?20220406173002"
+                        src="https://codidatabucket.s3.ap-northeast-2.amazonaws.com/img/2022male/85259.jpg"
                         alt="NoImg"
                     />
                 </Col>
                 <Col span={12}>
                     <img
                         className="up"
-                        src="https://image.msscdn.net/mfile_s01/_street_images/72539/list.street_list_60e2962eefcb1.jpg.250?20210721141020"
+                        src="https://codidatabucket.s3.ap-northeast-2.amazonaws.com/img/2023female/92002.jpg"
                         alt="NoImg"
                     />
                 </Col>
                 <Col span={12}>
                     <img
                         className="down"
-                        src="https://image.msscdn.net/mfile_s01/_street_images/54972/list.street_list_5d7750d45ebc5.jpg.250?20200819082909"
+                        src="https://codidatabucket.s3.ap-northeast-2.amazonaws.com/img/2022male/84832.jpg"
                         alt="NoImg"
                     />
                 </Col>
