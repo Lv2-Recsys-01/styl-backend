6097c96ffbe2b01c5e14bfc4bddc6d16e486ccf1
junwon <jjuny9798@gmail.com>
2023-07-15 14:56:38 +0900

    chore: change scroll button

--- client/src/components/MoveBottom/index.jsx
+++ client/src/components/MoveBottom/index.jsx
@@ -1,12 +1,16 @@
 import React from "react";
-import { ArrowDownOutlined } from "@ant-design/icons";
+import { CaretDownFilled } from "@ant-design/icons";
 import "./MoveBottom.css";
 
 class MoveBottom extends React.Component {
+    handleScrollToBottom = () => {
+        document.documentElement.scrollTo({ top: document.documentElement.scrollHeight -10, behavior: "smooth" });
+    };
+    
     render() {
         return (
             <div className="scroll-to-bottom-button">
-                <ArrowDownOutlined className="bottom-arrow" />
+                <CaretDownFilled className="bottom-arrow" onClick={this.handleScrollToBottom}/>
             </div>
         );
     }
--- client/src/components/MoveToTop/index.jsx
+++ client/src/components/MoveToTop/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { UpCircleOutlined } from "@ant-design/icons";
+import { CaretUpFilled } from "@ant-design/icons";
 import "./MoveToTop.css";
 
 class MoveToTop extends React.Component {
@@ -10,7 +10,7 @@ class MoveToTop extends React.Component {
     render() {
         return (
             <div className="scroll-to-top-button">
-                <UpCircleOutlined className="top-arrow" onClick={this.handleScrollToTop} />
+                <CaretUpFilled className="top-arrow" onClick={this.handleScrollToTop} />
             </div>
         );
     }
