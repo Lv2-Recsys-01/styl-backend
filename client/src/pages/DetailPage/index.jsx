5834bb8e368864088e7d6628b4435c8cf9f05959
HanJeong Lee <54920378+leehanjeong@users.noreply.github.com>
2023-07-17 16:53:15 +0900

    Feat journey page gender ratio (#56)
    * chore: change toggler design
    
    * chore: add margin-top on main page
    
    * chore: adjust size
    
    * chore: change scroll button
    
    * chore: adjust detail page
    
    * chore: change p tag to div tag
    
    fix error
    
    * chore: change main header
    
    * feat: add move to top
    
    to show consistency view
    
    * chore: add use login information
    
    * chore: change scroll effect
    
    * feat: add fallback image
    
    * feat: add 404 page
    
    * fix: fetching error
    
    fix is_last
    
    * chore: fix touch button area
    
    to adjust click
    
    * chore: add scrool to top
    
    * feat: modify the gender ratio of the journey page to 1:1
    
    * chore: change is_last method
    
    Works on odd page_size as well
    
    ---------
    
    Co-authored-by: junwon <jjuny9798@gmail.com>
    Co-authored-by: darrenkwondev <darrenkwondev46@gmail.com>

--- client/src/pages/DetailPage/index.jsx
+++ client/src/pages/DetailPage/index.jsx
@@ -86,7 +86,7 @@ function DetailCodi() {
                     onError={(e) => {
                         e.target.onerror = null;
                         e.target.src = "https://codidatabucket.s3.ap-northeast-2.amazonaws.com/img/subimage/loading.jpg";
-                      }}
+                      }} 
                     />
                     <div className="options">
                         <a href={singleOutfit.origin_url}>
