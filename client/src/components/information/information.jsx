9242eee4b6d92e2892a3e05221930adc9ed4f0bd
junwon <jjuny9798@gmail.com>
2023-07-26 02:13:09 +0900

    feat: add animation on info

--- client/src/components/information/information.jsx
+++ client/src/components/information/information.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { CloseCircleFilled, BulbOutlined } from '@ant-design/icons';
 import './information.css';
  
-const MyComponent = () => {
+const MyComponent = (props) => {
   const [visible, setVisible] = useState(true);
 
   const handleToggle = () => {
@@ -14,8 +14,7 @@ const MyComponent = () => {
       {visible && (
         <div className="popup-container">
           <CloseCircleFilled className='popup-close'onClick={handleToggle} />
-          <h2 className='popup-text'><BulbOutlined className='bulb' />코디를 클릭해,</h2>
-          <h2 className='popup-text'><BulbOutlined className='bulb' />유사한 코디를 확인하세요!</h2>
+          <h2 className='popup-text'><BulbOutlined className='bulb' />하트를 눌러, 당신의 스타일을 찾아보세요!{props.text}</h2>
         </div>
       )}
     </div>
