d2b71c98b90608e42f0b117ad310b8d094987c7b
RonaldFisher9999 <1993sabre@gmail.com>
2023-07-25 18:04:13 +0900

    add similar rec

--- src/router/item_router.py
+++ src/router/item_router.py
@@ -1,4 +1,4 @@
-import random
+import numpy as np
 from datetime import datetime
 from typing import Annotated
 
@@ -19,50 +19,69 @@ router = APIRouter(
 )
 
 
-def get_recommendation(db: Session, likes: list | None, page_size: int, style_type: str) -> list:
+def get_recommendation(db: Session, likes: list | None, page_size: int,
+                       style_type: str='season_2',
+                       style_rec_cnt: int=4, sim_rec_cnt: int=4) -> list:
+    if style_rec_cnt + sim_rec_cnt > page_size:
+        style_rec_cnt = round(page_size * style_rec_cnt / (style_rec_cnt + sim_rec_cnt))
+        sim_rec_cnt = page_size - style_rec_cnt
     if style_type not in ['no_season', 'season_1', 'season_2']:
-        style_type = 'no_season'
+        style_type = 'season_2'
     
     outfits = list()
-    cand = list()
+    style_cand = list() # 카테고리 기반 추천 후보
+    sim_cand = list() # 유사 아이템 기반 추천 후보
     if likes:
         for like in likes:
             outfit_id = like.outfit_id
             outfit = db.query(Outfit).filter(Outfit.outfit_id == outfit_id).first()  # type: ignore
             if outfit:
                 style_id = getattr(outfit, f"{style_type}")
-                cand.append(style_id)
-        # 최대 8개 추천
-        recs = random.sample(cand, min(8, len(cand)))
-        for style_id in recs:
+                style_cand.append(style_id)
+            similar = db.query(Similar).filter(Similar.outfit_id == outfit_id).first()
+            if similar:
+                sim_cand.extend(similar.similar_outfits) # type: ignore
+
+        k = round(len(likes)/2)
+        style_recs = np.random.choice(style_cand, size=min(style_rec_cnt, k)).tolist()
+        unique_outfits, counts = np.unique(sim_cand, return_counts=True)
+        sim_recs = np.random.choice(unique_outfits, size=min(sim_rec_cnt, k),
+                                replace=False, p=counts/counts.sum()).tolist()
+        
+        for style_id in style_recs:
             outfit = db.query(Outfit).filter(getattr(Outfit, f"{style_type}") == style_id).order_by(func.random()).first()
             if outfit:
                 outfits.append(outfit)
-
+        for outfit_id in sim_recs:
+            outfit = db.query(Outfit).filter(Outfit.outfit_id==outfit_id).first()
+            if outfit:
+                outfits.append(outfit)
+        # print("outfits:", [o.outfit_id for o in outfits])
+        # print("cnt:", len([o.outfit_id for o in outfits]))
     if len(outfits) < page_size:
-        n = page_size - len(outfits)
+        k = page_size - len(outfits)
         
         f_outfits = (
             db.query(Outfit)
             .filter(Outfit.gender == "F")
             .order_by(func.random())
-            .limit(n // 2)
+            .limit(k // 2)
             .all()
         )
         m_outfits = (
             db.query(Outfit)
             .filter(Outfit.gender == "M")
             .order_by(func.random())
-            .limit(n - (n // 2))
+            .limit(k - (k // 2))
             .all()
         )
         outfits += f_outfits + m_outfits
     
-    random.shuffle(outfits)
-
+    # print("final outfits:", [o.outfit_id for o in outfits])
+    # print("cnt:", len([o.outfit_id for o in outfits]))
+    np.random.shuffle(outfits)
     return outfits
 
-      
 
 @router.get("/journey")
 def show_journey_images(
@@ -97,7 +116,7 @@ def show_journey_images(
         )
     # style_type should be 'no_season', 'season_1', or 'season_2'
     # season_1: 4계절 전부 구분, season_2: 개수 적은 카테고리는 합침
-    outfits = get_recommendation(db, likes, page_size, style_type='season_2')
+    outfits = get_recommendation(db, likes, page_size)
 
     # 마지막 페이지인지 확인
     is_last = len(outfits) < page_size
