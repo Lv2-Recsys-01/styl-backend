fe65872e63c36fff6d7870c732fda80dec9b97e5
DarrenKwonDev <darrenkwondev46@gmail.com>
2023-07-26 11:03:19 +0900

    chore: outfit

--- scripts/insert_similar.py
+++ scripts/insert_similar.py
@@ -10,7 +10,7 @@ user = "postgres"
 password = "password"
 
 
-csv_file = os.path.join(os.path.dirname(__file__), "../top3_21-23.csv")
+csv_file = os.path.join(os.path.dirname(__file__), "../similar_filter.csv")
 
 conn = psycopg2.connect(
     host=host,
@@ -24,9 +24,10 @@ cursor = conn.cursor()
 with open(csv_file, "r") as f:
     reader = csv.DictReader(f)
     for row in reader:
-        outfit_id = int(row["id"])
+        outfit_id = int(row["outfit_id"])
         similar_outfits = [
-            int(outfit.strip()) for outfit in row["top3"].strip("[]").split(",")
+            int(outfit.strip())
+            for outfit in row["similar_outfits"].strip("[]").split(",")
         ]
 
         # outfit_id(FK)가 "outfit" 테이블에 존재하는지 확인
